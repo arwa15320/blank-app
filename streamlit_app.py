5446a389e634fc8de2e9bd26c9338402bde088d4
arwa15320 <97008135+arwa15320@users.noreply.github.com>
2024-08-23 19:09:13 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title("🎈 My mariam VS  arwa app2")
 st.write("hey , customer")
 f1=st.number_input("feature 1",min_value=1,max_value=10)
 f2=st.number_input("feature 2",min_value=1,max_value=10)
-f3=st.number_input("feature 3",min_value=1,max_value=10)
+f3=st.number_input("feature 3",min_value=1,max_value=100)
 
 with open('model.pkl','rb') as file:
     model=pickle.load(file)
