7f8a6fc8c922b82a468e2806b2c27348a36fe515
arwa15320 <97008135+arwa15320@users.noreply.github.com>
2024-08-23 19:16:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,6 @@ f3=st.number_input("feature 3",min_value=1,max_value=100)
 with open('model.pkl','rb') as file:
     model=pickle.load(file)
 result=model.predict([[f1,f2,f3]])
-st.button("hhhh")
+st.button("show")
 st.write("the predict value is : ")
 st.write(result)
