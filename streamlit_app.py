f9a0e53a81be0ee7a3c300b204057b4dc1a3360b
arwa15320 <97008135+arwa15320@users.noreply.github.com>
2024-08-23 19:14:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import  pickle
 st.title("🎈 My mariam VS  arwa app2")
-st.write("hey , customer")
+st.header("hey , customer")
 f1=st.number_input("feature 1",min_value=1,max_value=10)
 f2=st.number_input("feature 2",min_value=1,max_value=10)
 f3=st.number_input("feature 3",min_value=1,max_value=100)
