640f18619166554f6ee952967c53f11727cfd5b5
arwa15320 <97008135+arwa15320@users.noreply.github.com>
2024-08-23 18:58:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit as st
 import  pickle
 st.title("🎈 My new app2")
 f1=st.number_input("feature 1",min_value=1,max_value=10)
-f2=st.number_input("feature 1",min_value=1,max_value=10)
-f3=st.number_input("feature 1",min_value=1,max_value=10)
+f2=st.number_input("feature 2",min_value=1,max_value=10)
+f3=st.number_input("feature 3",min_value=1,max_value=10)
 
 with open('model.pkl','rb') as file:
     model=pickle.load(file)
